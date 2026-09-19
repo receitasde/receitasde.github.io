c3e2c269956c27b3d610478cc72b6c319fcada74
criarsite <oktuga@gmail.com>
2026-09-19 00:16:47 +0000

    ads

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,7 +4,7 @@ module.exports = {
   url: "https://verefazer.com",
   description:
     "Ferramentas online gratuitas de texto, conversão, geração, cor, cálculo e desenvolvimento. Tudo roda no seu navegador, sem instalar nada e sem enviar seus dados a um servidor.",
-  adsenseClient: "ca-pub-560389560002412",
+  adsenseClient: "ca-pub-5603895600024124",
   adsenseSlot: "7425818662",
   adsenseEnabled: true,
   email: "contato@verefazer.com",
